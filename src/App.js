617c018e9715723732d5312452ec43ce6e754525
Rajesh Reddy <rajeshreddy@rajeshs-air-2.home>
2024-10-30 22:36:10 -0400

    updated app.js, about.js and about.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Nav";
 import Home from "./Components/Home";
+import About from "./Components/About";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
            <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
---
+++ src/Components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './About.css';
+
+function About() {
+  const showFunFact = () => {
+    alert("Fun Fact: Did you know that 0 is the only real number that is neither positive nor negative?");
+  };
+
+  return (
+    <div classname="home-container">
+      <h1>About Us</h1>
+      <p>This app includes a number guessing game, a task list, and a contact form.</p>
+      <button onClick={showFunFact}>Show Fun Fact</button>
+    </div>
+  );
+}
+
+export default About;
